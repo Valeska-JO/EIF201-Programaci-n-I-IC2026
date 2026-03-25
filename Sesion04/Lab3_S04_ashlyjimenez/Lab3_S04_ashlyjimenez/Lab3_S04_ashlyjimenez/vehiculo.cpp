56c39cc13441c7762f3b07523d85bf30b7f94fbd
Ashly Jimenéz Ortíz <ashly.jimenez.ortiz@est.una.ac.cr>
2026-03-25 01:19:47 -0600

    Laboratorio 4
    Elaboracion de clase vehiculo

--- Sesion04/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/vehiculo.cpp
+++ Sesion04/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/vehiculo.cpp
@@ -1,1 +1,4 @@
 #include "vehiculo.h"
+namespace UNA {
+vehiculo::vehiculo(std::string p, std::string m, int a, double k) : placa(p), year(a), marca(m), kilometraje(k), activo(true){}
+}
--- Sesion04/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/vehiculo.h
+++ Sesion04/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/Lab3_S04_ashlyjimenez/vehiculo.h
@@ -1,5 +1,25 @@
 #pragma once
-class vehiculo
-{
-};
-
+#ifndef VEHICULO_H
+#define VEHICULO_H
+#include <string>
+#include <iostream>
+namespace UNA {
+	class vehiculo{
+	private:
+		std::string placa;
+	    std::string marca;
+		double kilometraje;
+		bool activo;
+	public:
+		vehiculo(std::string p, std::string m, int a, double k);
+		int year;
+		std::string getPlaca() const;
+		std::string getMarca() const;
+		bool isActivo() const;
+		void registrarKilometros(double km);
+		void desactivar();
+		void reactivar();
+		void mostrar() const;
+	};
+}
+#endif
